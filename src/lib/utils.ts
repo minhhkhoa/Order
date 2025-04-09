65f4987b1860f3133f30dfeb169277a4aeec54e7
Khoa <khoanguyen.1881@gmail.com>
2025-04-09 20:32:36 +0700

    config http client to call api

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+//- loại bỏ ký tự "/" ở đầu chuỗi nếu nó tồn tại
+export const normalizePath = (path: string) => {
+  return path.startsWith("/") ? path.slice(1) : path;
+};
+
