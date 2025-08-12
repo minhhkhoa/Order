1114f300914113c59b8142e2418e817bf9f77921
Khoa <khoanguyen.1881@gmail.com>
2025-08-12 16:43:36 +0700

    UI login

---
+++ client/src/app/(public)/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from '@/app/(public)/(auth)/login/login-form'
+
+export default function Login() {
+  return (
+    <div className="translate-y-1/2 md:translate-y-1/4 flex items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+}
