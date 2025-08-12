e5b4c79112b6614450f16f6cb81838adb8388980
Khoa <khoanguyen.1881@gmail.com>
2025-08-12 16:48:21 +0700

    Update route.ts

--- client/src/app/api/auth/login/route.ts
+++ client/src/app/api/auth/login/route.ts
@@ -1,4 +1,4 @@
-import authApiRequest from "@/apirequests/auth";
+import authApiRequest from "@/apiRequests/auth";
 import { HttpError } from "@/lib/http";
 import { LoginBodyType } from "@/schemaValidations/auth.schema";
 import jwt from "jsonwebtoken";
